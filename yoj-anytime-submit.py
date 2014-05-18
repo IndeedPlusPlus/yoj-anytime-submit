da3e75697adafdb70942c6c9c760e24cd6086171
Indeed <indeed@indeedblog.net>
2014-05-18 17:40:00 +0800

    add file existence check

--- yoj-anytime-submit.py
+++ yoj-anytime-submit.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 import getpass
+import os.path
 s = requests.Session()
 web_root = raw_input("Website root [http://202.112.113.8]: " )
 if not web_root:
@@ -17,9 +18,14 @@ except:
 assignment_id = int(raw_input("Assignment ID: "))
 filename = raw_input("File to send: ").decode(sys.stdin.encoding);
 if not filename:
-	print >> sys.stderr, "ERROR: You must enter a file name"
+	print >> sys.stderr, "ERROR: You must enter a file name."
+	quit()
+if not os.path.isfile(filename):
+	print >> sys.stderr, "ERROR: %s does not exist or is not a file."
 	quit()
 print >> sys.stderr, "Submitting your work, please wait."
 r = s.post(web_root + '/assignment.php?id=%d'%assignment_id, data = {'uploadthisafile':'uploadthisafile'}, files = {'file' : open(filename, 'rb') })
 r.raise_for_status()
+print r.text
 print >> sys.stderr, "Your work has been submitted."
+
